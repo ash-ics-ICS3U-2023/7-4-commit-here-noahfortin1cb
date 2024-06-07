7104fcf484c57db0f5e62889da1fd8e8cdbbaa8f
Noah Fortin <noah.fortin@stu.ocsb.ca>
2024-06-07 13:51:56 -0400

    comit

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
-/**
- * This file will get loaded by index.html
- */
+
+let clicks = 0;
+
+document.getElementById("click1").addEventListener("click", add);
+document.getElementById("click2").addEventListener("click", sub);
+document.getElementById("click3").addEventListener("click", reset);
+
+function add() {
+   clicks++;
+   document.getElementById("clickCount").textContent = clicks;
+}
+
+function sub() {
+   clicks--;
+   document.getElementById("clickCount").textContent = clicks;
+}
+
+function reset() {
+   clicks = 0;
+   document.getElementById("clickCount").textContent = clicks;
+}
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,2 @@
+
+const clickCountSpan = document.getElementById
